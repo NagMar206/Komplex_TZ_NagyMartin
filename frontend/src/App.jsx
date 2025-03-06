5b20b378ee721071005b3680fb257288326398dc
NagMar206 <nagmar206@hengersor.hu>
2025-03-06 12:38:59 +0100

    Táblázat működik

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,28 @@ import './App.css'
 import './felveteli.css'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js"
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import 'tachyons'
+
 
 function App() {
+
+  const [data, setData] = useState([]);
+ 
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/diakok")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Nem helyes a válasz a hálózattól");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => console.error("Hiba:", error));
+  }, []);
 
   return (
     <>
@@ -16,7 +36,7 @@ function App() {
       <div className='container-fluid row'>
             <div className='bg-torzs col-md-4'>
               <h3>Központi felvételi tájékoztató</h3>
-                <p>A középiskolákba történő jelentkezés során az iskolák határozzák meg, hogy a felvételi rangsort mi alapján döntik el. A Jószakma Szakgimnázium a felvételi során az általános iskolából hozott és a központi felvételin szerzett pontok alapján rangsorolja az iskolába jelentkezőket. </p>
+                <p className='text-left'>A középiskolákba történő jelentkezés során az iskolák határozzák meg, hogy a felvételi rangsort mi alapján döntik el. A Jószakma Szakgimnázium a felvételi során az általános iskolából hozott és a központi felvételin szerzett pontok alapján rangsorolja az iskolába jelentkezőket. </p>
                 <a href="https://www.oktatas.hu/kozneveles/kozepfoku_felveteli_eljaras/kozepfoku_felveteli_eljaras_informacioi">Tájékoztató oldal</a>
                 <img src="./src/assets/logo.png" alt="" />
 
@@ -46,11 +66,10 @@ function App() {
 
 
 <div>
-<div>
-    <div className='bg-torzs col-md-4'>
-        <h3>A felvételt nyert tanulók névsora a nyelvi előkészítőre</h3>
+<div className='row'>
+    <div className='col-md-5'>
+        <h3 className='text-left'>A felvételt nyert tanulók névsora a nyelvi előkészítőre</h3>
         <div>
-
         </div>
     </div>
 <table>
@@ -63,8 +82,20 @@ function App() {
     </tr>
     </thead>
     <tbody>
-      
-    </tbody>
+          {data.length > 0 ? (
+            data.map((diak, index) => (
+              <tr key={index}>
+                <td>{diak.nev}</td>
+                <td>{diak.agazat}</td>
+                <td>{diak.osszpont}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="3" className="text-center">Nincs elérhető adat</td>
+            </tr>
+          )}
+        </tbody>
 </table>
 </div>
 </div>
